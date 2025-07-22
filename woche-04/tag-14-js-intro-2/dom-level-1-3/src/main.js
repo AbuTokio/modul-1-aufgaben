65889e48174213c7a1022c7b2339d40eb6e222bf
AbuTokio <bunyaminxkara@gmail.com>
2025-07-22 13:01:07 +0200

    DOM - Level 1-3

---
+++ woche-04/tag-14-js-intro-2/dom-level-1-3/src/main.js
@@ -0,0 +1,16 @@
+const gallery = document.querySelector("#gallery")
+
+gallery.innerHTML += `<figure>
+        <img src="./img/cat.jpg" height="300px" alt="Cat" />
+        <figcaption>Fig. 1</figcaption>
+      </figure>`
+
+gallery.innerHTML += `<figure>
+        <img src="./img/cat.jpg" height="300px" alt="Cat" />
+        <figcaption>Fig. 2</figcaption>
+      </figure>`
+
+gallery.innerHTML += `<figure>
+        <img src="./img/cat.jpg" height="300px" alt="Cat" />
+        <figcaption>Fig. 3</figcaption>
+      </figure>`
