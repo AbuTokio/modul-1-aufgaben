2026589e06c4c8fa76f041daef7b60d3e18e6bab
AbuTokio <bunyaminxkara@gmail.com>
2025-07-23 13:02:52 +0200

    Number Methods - Level 1-2

---
+++ woche-04/tag-15-js-methoden/number-methods-level-1-2/src/main.js
@@ -0,0 +1,15 @@
+const val1 = 15.16698
+const val2 = 7.78714
+const val3 = "12.3"
+const val4 = true
+const val5 = false
+const val6 = "321"
+const val7 = "Supercode"
+
+console.log(val1.toFixed(2))
+console.log(val2.toFixed(2))
+console.log(Number(val3).toFixed(2))
+console.log(Number(val4).toFixed(2))
+console.log(Number(val5).toFixed(2))
+console.log(Number(val6).toFixed(2))
+console.log(Number(val7).toFixed(2))
