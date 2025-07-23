08108665995ccef2372759705a2a92b3551c1d18
AbuTokio <bunyaminxkara@gmail.com>
2025-07-23 12:37:45 +0200

    Math - Level 1-3

---
+++ woche-04/tag-15-js-methoden/math-level-1-3/src/main.js
@@ -0,0 +1,7 @@
+const randomNum = Math.random()
+const randomNum1_10 = Math.floor(Math.random() * 11)
+const randomNum1_100 = Math.floor(Math.random() * 101)
+
+console.log(randomNum)
+console.log(randomNum1_10)
+console.log(randomNum1_100)
