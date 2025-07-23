0900c1dade05ea4030c89a6552259646a00267ec
AbuTokio <bunyaminxkara@gmail.com>
2025-07-23 12:33:44 +0200

    Math - Level 1-2

---
+++ woche-04/tag-15-js-methoden/math-level-1-2/src/main.js
@@ -0,0 +1,10 @@
+function roundNumber(x) {
+  console.log(Math.round(x))
+}
+
+const randomNumbers = [3.14, 193.4464, 0.8596433607, -2.940629089]
+
+roundNumber(randomNumbers[0])
+roundNumber(randomNumbers[1])
+roundNumber(randomNumbers[2])
+roundNumber(randomNumbers[3])
