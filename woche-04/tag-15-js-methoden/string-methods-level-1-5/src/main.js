945b443b12350d7d7f0398ef52afdb50386acae0
AbuTokio <bunyaminxkara@gmail.com>
2025-07-23 12:27:46 +0200

    String Methods - Level 1-5

---
+++ woche-04/tag-15-js-methoden/string-methods-level-1-5/src/main.js
@@ -0,0 +1,11 @@
+const whereIsSusi = "Susi is back from codingschool"
+
+const myVar1 = whereIsSusi.substring(0, 4)
+const myVar2 = whereIsSusi.substring(5, 7)
+const myVar3 = whereIsSusi.substring(24, 30)
+const myVar4 = whereIsSusi.substring(0, 5) + whereIsSusi.substring(5, 8) + whereIsSusi.substring(24, 30)
+
+document.write(myVar1 + "  ")
+document.write(myVar2 + "  ")
+document.write(myVar3 + "  ")
+document.write(myVar4 + "  ")
