13dc06e7cf985f27f46025cc2191356c6f46d0bd
AbuTokio <bunyaminxkara@gmail.com>
2025-07-23 13:15:19 +0200

    Number Methods - Level 1-4

---
+++ woche-04/tag-15-js-methoden/number-methods-level-1-4/src/main.js
@@ -0,0 +1,29 @@
+const val1 = 15.16698
+const val2 = 7.78714
+const val3 = "12.3"
+const val4 = "3.14random"
+const val5 = "32px"
+const val6 = true
+const val7 = false
+const val8 = "321"
+const val9 = "Supercode"
+
+console.log(Number(val1))
+console.log(Number(val2))
+console.log(Number(val3))
+console.log(Number(val4))
+console.log(Number(val5))
+console.log(Number(val6))
+console.log(Number(val7))
+console.log(Number(val8))
+console.log(Number(val9))
+
+console.log(parseFloat(val1))
+console.log(parseFloat(val2))
+console.log(parseFloat(val3))
+console.log(parseFloat(val4))
+console.log(parseInt(val5))
+console.log(parseInt(val6))
+console.log(parseInt(val7))
+console.log(parseInt(val8))
+console.log(parseInt(val9))
